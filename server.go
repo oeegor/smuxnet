28ec08c64400fa4d418e16613e426f6c253fe7e2
Egor Orlov <egor.orlov@ostrovok.ru>
2017-04-21 09:30:51 +0300

    [master]

--- server.go
+++ server.go
@@ -1,10 +1,11 @@
 package smuxnet
 
 import (
+	"io"
 	"net"
 	"time"
 
-	"io"
+	"fmt"
 
 	"github.com/xtaci/smux"
 	"github.com/zenhotels/chanserv"
@@ -27,28 +28,48 @@ func (s *server) ListenAndServe(addr string, srcFn chanserv.SourceFunc) error {
 	if err != nil {
 		return err
 	}
-	conn, err := listener.Accept()
-
-	// Setup server side of smux
-	session, err := smux.Server(conn, nil)
 	for {
-		stream, err := session.AcceptStream()
+		// new node has connected to us
+		conn, err := listener.Accept()
 		if err != nil {
 			return err
 		}
-		go s.processStream(stream, srcFn)
-
+		go s.serve(conn, srcFn)
 	}
+
 	return nil
 }
 
+func (s *server) serve(conn net.Conn, srcFn chanserv.SourceFunc) {
+	// Setup server side of smux
+	session, _ := smux.Server(conn, nil)
+	for {
+		stream, err := session.AcceptStream()
+		if err != nil {
+			fmt.Println("error accepting stream", err)
+			break
+		}
+		go s.processStream(stream, srcFn)
+	}
+}
+
 func (s *server) processStream(stream *smux.Stream, srcFn chanserv.SourceFunc) {
+	if s.writeTimeout > 0 {
+		stream.SetWriteDeadline(time.Now().Add(s.writeTimeout))
+	}
+	if s.readTimeout > 0 {
+		stream.SetReadDeadline(time.Now().Add(s.readTimeout))
+	}
+
 	buf := make([]byte, 1024)
 	io.ReadFull(stream, buf)
 	for src := range srcFn(buf) {
 		go func(s chanserv.Source) {
 			for frame := range src.Out() {
-				stream.Write(frame.Bytes())
+				_, err := stream.Write(frame.Bytes())
+				if err != nil {
+					fmt.Println("error writing to stream", err)
+				}
 			}
 		}(src)
 	}
